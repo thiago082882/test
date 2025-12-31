f937552e15469be3ecfe850794409a66013e85ce
thiago082882 <thiagocostadejesus2@gmail.com>
2025-12-31 13:18:56 -0300

    first commit

--- app/src/main/java/br/thiago/estudoskotlin/advanced/oop/Enums.kt
+++ app/src/main/java/br/thiago/estudoskotlin/advanced/oop/Enums.kt
@@ -2,7 +2,7 @@ package br.thiago.estudoskotlin.advanced.oop
 
 
 //https://www.youtube.com/watch?v=caChcOkk7mA
-
+//https://github.com/thiago082882/test
 enum class AccountType(val rate : Double){
 
 }
